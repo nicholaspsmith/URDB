4e6212b57eacd962b608154ee9a26b14ff782461
Nick P Smith <npsmith1990@gmail.com>
2013-10-29 17:13:57 -0500

    change movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,8 +4,6 @@ class Movie < ActiveRecord::Base
   include Api
   def get_attributes
     api_key = ApiKey.new
-    puts 'XXXXXXXXXXXXX'
-    puts api_key.value
     movie = Unirest::get("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{api_key.value}&q=#{self.title}&page_limit=1") 
     movie.body['movies'][0]
   end
